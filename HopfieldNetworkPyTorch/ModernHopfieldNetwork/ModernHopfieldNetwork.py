df2341bb4593691b51052818d1247e6080e2c887
Hayden McAlister <haydenmcalister49@gmail.com>
2024-03-26 15:00:12 +1300

    Update modern hopfield impl

--- HopfieldNetworkPyTorch/ModernHopfieldNetwork/ModernHopfieldNetwork.py
+++ HopfieldNetworkPyTorch/ModernHopfieldNetwork/ModernHopfieldNetwork.py
@@ -22,16 +22,19 @@ class ModernHopfieldNetwork():
         """
         Set the memories of the network directly. Note the memories must be moved to the preferred device before being passed.
 
-        :param memories: The new memories of the network. Must be of shape (network.dimension, network.nMemories) and be moved to the preferred device.
+        :param memories: The new memories of the network. Must be of shape (network.dimension, nMemories) and be moved to the preferred device.
         """
         if memories.shape != self.memories.shape:
-            raise ValueError("memories should have shape (network.dimension, network.nMemories)")
+            raise ValueError("memories should have shape (network.dimension, nMemories)")
 
+        memories = memories.to(self.memories.device)
         self.memories = memories
     
     def learnMemories(self, X: torch.Tensor, learningRate: float, ):
         """
         TODO: Implement backprop to learn memories representing training data.
+
+        :param X: States, a tensor of shape (network.dimension, nStates).
         """
 
         raise NotImplementedError
@@ -42,54 +45,63 @@ class ModernHopfieldNetwork():
         Energy is calculated as the interaction function applied to the sum of all memories dot the state.
         -F(sum M.T @ x)
 
-        Note X must have shape (self.dimension, n) where n is the number of states 
+        :param X: States, a tensor of shape (network.dimension, nStates).
         """
 
-        return -self.interactionFunction(self.memories.T @ X).sum(axis=0)
+        return -self.interactionFunction(torch.sum(self.memories.T @ X, axis=0))
+    
+    def stable(self, X: torch.Tensor):
+        """
+        Calculate the stability of each state given.
+
+        :param X: States, a tensor of shape (network.dimension, nStates).
+        :returns: A (nStates) tensor of booleans with each entry the stability of a state.
+        """
 
     def stepStates(self, X: torch.Tensor):
         """
         Step the given states according to the energy difference rule. 
         Step implies only a single update is made, no matter if the result is stable or not.
 
-        Note X must have shape (self.dimension, n) where n is the number of states to update
+        Note X must have shape (self.dimension, nStates) where n is the number of states to update
         X must already be moved to the correct device. This can be done with X.to(network.device)
 
         :param X: The tensor of states to step. 
-            Tensor must be on the correct device and have shape (dimension, n), for any n
+            Tensor must be on the correct device and have shape (network.dimension, nStates)
         """
 
         if X.device != self.memories.device:
             print(f"ERR: State device ({X.device}) does not match network device ({self.memories.device}). No computation performed.")
             return
 
-        updateOrder = np.arange(0, self.dimension)
-        np.random.shuffle(updateOrder)
+        updateOrder = np.arange(self.dimension)
+        # np.random.shuffle(updateOrder)
 
-        for i in updateOrder:
-            X_inverse = X.clone()
-            X_inverse[i, :] *= -1
-            energyDifference = self.energy(X_inverse) - self.energy(X)
-            # print(i, energyDifference)
-            
-            # If the energy is lower (energy difference is negative) then flip
-            # Note torch.where selects from the second arg when predicate is true, third when false
-            X[i, :] = torch.where(energyDifference>=0, X[i, :], X_inverse[i, :])
+        for neuronIndex in updateOrder:
+            x0 = X.clone()
+            x0[neuronIndex, :] = 0
+            e0 = self.memories.T @ x0
 
-    def updateStates(self, X: torch.Tensor, maxIterations: int = 100):
+            memoryRow = self.memories[neuronIndex, :].unsqueeze(1)
+            energyDifference = self.interactionFunction(memoryRow + e0) - self.interactionFunction(-memoryRow + e0)
+            energyDifference = energyDifference.sum(axis=0)
+            X[neuronIndex, :] = torch.where(energyDifference>=0, 1, -1)
+
+    def relaxStates(self, X: torch.Tensor, maxIterations: int = 100, verbose: bool = False):
         """
         Update the states some number of times.
 
         :param X: The tensor of states to step. 
-            Tensor must be on the correct device and have shape (dimension, n), for any n
+            Tensor must be on the correct device and have shape (network.dimension, nStates)
         :param maxIterations: The integer number of iterations to update the states for.
+        :param verbose: Flag to show progress bar
         """
 
         if X.device != self.memories.device:
             print(f"ERR: State device ({X.device}) does not match network device ({self.memories.device}). No computation performed.")
             return
-
-        for _ in tqdm(range(maxIterations)):
+        
+        for _ in tqdm(range(maxIterations), desc="Relax States", disable=not verbose):
             X_prev = X.clone()
             self.stepStates(X)
             if torch.all(X_prev == X):
