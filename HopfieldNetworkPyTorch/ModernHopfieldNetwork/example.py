25bf97a4487e65dc9b6bfb52256a9cc10dc9799a
Hayden McAlister <mcaha814@student.otago.ac.nz>
2024-03-27 16:51:15 +1300

    Update example with gradient descent impl

--- HopfieldNetworkPyTorch/ModernHopfieldNetwork/example.py
+++ HopfieldNetworkPyTorch/ModernHopfieldNetwork/example.py
@@ -8,13 +8,13 @@ def createBipolarStates(dimension: int, nStates: int):
     return X
 
 dimension = 100
-nMemories = 50  
+nMemories = 10
 nStates = 100
 memories = createBipolarStates(dimension, nMemories).to(device)
 X = createBipolarStates(dimension, nStates).to(device)
+interactionFunc = InteractionFunction.RectifiedPolynomialInteractionFunction(n=3)
 
-
-interactionFunc = InteractionFunction.PolynomialInteractionFunction(n=4)
+# Direct Memory Storage
 network = ModernHopfieldNetwork(dimension, nMemories, device, interactionFunc)
 network.setMemories(memories)
 
@@ -22,4 +22,26 @@ x = X.clone()
 network.relaxStates(x)
 similarities = torch.abs(memories.T @ x)
 mostSimilar = torch.max(similarities, axis=0).values
-print(torch.mean(mostSimilar), torch.std(mostSimilar))
+print("Direct Memory Storage")
+print("\tFinal State Average Similarity to Memory:\t", torch.mean(mostSimilar).item())
+print("\tFinal State Average Standard Deviation of Similarity:\t", torch.std(mostSimilar).item())
+
+# Learned Memories
+network = ModernHopfieldNetwork(dimension, 10*nMemories, device, interactionFunc)
+network.learnMemories(memories, 
+                      maxEpochs=250,
+                      learningRate=0.05, 
+                      learningRateDecay=0.999, 
+                      momentum=0.8, 
+                      beta=0.01,
+                      errorPower=1,
+                      verbose=1
+                      )
+
+x = X.clone()
+network.relaxStates(x)
+similarities = torch.abs(memories.T @ x)
+mostSimilar = torch.max(similarities, axis=0).values
+print("Learned Memory Storage")
+print("\tFinal State Average Similarity to Memory:\t", torch.mean(mostSimilar).item())
+print("\tFinal State Average Standard Deviation of Similarity:\t", torch.std(mostSimilar).item())
