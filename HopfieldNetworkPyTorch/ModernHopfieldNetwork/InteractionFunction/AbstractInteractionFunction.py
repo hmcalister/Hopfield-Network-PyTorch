9928342249347189b3a1b0bf6e8b0862df1d089f
Hayden McAlister <mcaha814@student.otago.ac.nz>
2024-04-05 14:41:18 +1300

    Add documentation to AbstractInteractionFunction

--- HopfieldNetworkPyTorch/ModernHopfieldNetwork/InteractionFunction/AbstractInteractionFunction.py
+++ HopfieldNetworkPyTorch/ModernHopfieldNetwork/InteractionFunction/AbstractInteractionFunction.py
@@ -5,7 +5,9 @@ class AbstractInteractionFunction(ABC):
     
     def __init__(self, n: int):
         """
-        Set the interaction vertex for the interaction function
+        Set the interaction vertex for the interaction function.
+
+        :param n: The interaction vertex of the activation function
         """
         
         self.n = n
@@ -13,3 +15,4 @@ class AbstractInteractionFunction(ABC):
     @abstractmethod
     def __call__(self, X: torch.Tensor) -> torch.Tensor:
         pass
+    
