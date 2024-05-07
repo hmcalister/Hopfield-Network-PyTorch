a865938d84f6c5d153d1fa53dc392ad6239b4629
Hayden McAlister <mcaha814@student.otago.ac.nz>
2024-05-07 12:53:50 +1200

    Update memory init to use float64 and initially clamp memories

--- HopfieldNetworkPyTorch/ModernHopfieldNetwork/ModernHopfieldNetwork.py
+++ HopfieldNetworkPyTorch/ModernHopfieldNetwork/ModernHopfieldNetwork.py
@@ -24,6 +24,7 @@ class ModernHopfieldNetwork():
         
         self.dimension = dimension
         self.memories = torch.rand(size=(self.dimension, nMemories), requires_grad=True, device=torchDevice, dtype=torch.float64)
+        self.memories.clamp(-1,1)
         self.interactionFunction = interactionFunction
 
         self.itemBatchSize = itemBatchSize
